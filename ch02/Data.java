3184249444f7e3d72fefe40e307415c75effbf3b
dorisjoh <dorisjoh@gmail.com>
2017-11-29 16:51:03 -0500

    Update Data.java

--- ch02/Data.java
+++ ch02/Data.java
@@ -1,50 +1,54 @@
 /**
  * Examples from Chapter 2.
  */
-public class TimeMidnight {
+public class TimeFromMidnight {
 
     public static void main(String[] args) {
         
-        //Variables Declard for public class "Data"
-        String timeCalc;
-        double hour, minute, second, midnight;
+        //Variables Declared for public class "Time"
+        double sHour, sMinute, sSecond, mid;
+        double eHour, eMinute, eSecond;
+        double timeCalc1, timeCalc2, timeCalc3, perPassed1;
+        double y;
         
         //Data assigned to Variables
-        hour = 12;
-        minute = 58;
-        second = 30;
-        midnight = 24;
+        sHour = 16;
+        sMinute = 00;
+        sSecond = 12;
+        mid = 24 *3600;
         
-        //Time Calculations
-        timeCalc = (hour *3600 + minute *60 + second) - midnight;
+        //Secondary Variables
+        eHour = 16;
+        eMinute = 38;
+        eSecond = 29;
+       
+        //Time Calculations...Time Since Midnight
+        timeCalc1 = (((sHour * 3600) + (sMinute * 60) + sSecond));
+        System.out.println("Time Since Midnight: " + timeCalc1);   
+                     
+         //Time Calculations...Time Until Midnight
+        timeCalc2 = (mid - timeCalc1);
+        System.out.println("Time Until Midnight: " + timeCalc2);
+                     
+        //Percentage of time since Midnight
+        perPassed1 = ((timeCalc1/mid) * 100);
+        System.out.printf("Percentage of time passed: %.2f", perPassed1);
+        System.out.println("%");
         
-        System.out.println(timeCalc);
- 
-      
+        //Elapsed Time...Since Beginning
+        timeCalc3 = (((eHour * 3600) + (eMinute * 60) + eSecond) - timeCalc1);
+        System.out.println("Time Elapsed Since Beginning this Assignment: " + timeCalc3);
+        
+        
+        //Calucation To Check Math
+        y = (timeCalc1 + timeCalc2);
+        
+        if (mid == y) {
+            System.out.println("The Math is Correct!");
+        }
+        else System.out.println ("Check the Math!");
     }
 
 }
 
-public class TimeRemaining {
-
-    public static void main(String[] args) {
-        
-        //Variables Declard for public class "Data"
-        String timeCalc;
-        double hour, minute, second, midnight;
-        
-        //Data assigned to Variables
-        hour = 12;
-        minute = 58;
-        second = 30;
-        midnight = 24;
-        
-        //Time Calculations
-        timeCalc = (hour *60 + minute *60 + second) - midnight;
-        
-        System.out.println(timeCalc);
- 
-      
-    }
-
-}
+                  
